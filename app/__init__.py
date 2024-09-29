3eae2a3779e872291ad9a9ca506e440dd6d7ef54
jhw <jhwerrr@gmail.com>
2024-09-29 11:15:39 +0800

    [backend] fix the db connection problem

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from app.router.user_router import user_router
 
 app = Flask("instance")
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql_KMmwMw@34.169.24.14/instance_ai'
 db = SQLAlchemy()
 db.init_app(app)
 
